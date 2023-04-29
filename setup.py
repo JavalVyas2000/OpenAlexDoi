66d92a877ae727d9222f88c93e870d7c7a9563ea
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-28 22:42:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+"""
+This is the setup file and has the config details for the package.
+"""
 from setuptools import setup
 
 setup(name='openalex',
       version='0.0.1',
-      
       description='bibtex and ris from Doi',
       maintainer='Javal Vyas',
       maintainer_email='jvyas@andrew.cmu.edu',
       license='MIT',
-      packages=['openalex'],
-      scripts=[],
+      packages=['OpenAlex'],
       setup_requires=["nose>=1.0"],
       install_requires=["bibtexparser","IPython"],
       entry_points={
