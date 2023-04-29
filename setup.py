39decef6f60f566c34bfca8703037f13aa310a3f
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-28 22:26:38 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='openalex',
+      version='0.0.1',
+      
+      description='bibtex and ris from Doi',
+      maintainer='Javal Vyas',
+      maintainer_email='jvyas@andrew.cmu.edu',
+      license='MIT',
+      packages=['openalex'],
+      scripts=[],
+      setup_requires=["nose>=1.0"],
+      install_requires=["bibtexparser","IPython"],
+      entry_points={
+        "console_scripts": ["cite = openalex.cmdline:cite"]},
+      long_description='''to get citations bibtex/ris''')
