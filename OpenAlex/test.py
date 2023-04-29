d2e6e12a648fbd7f32f1a17ce9377d35b3e5db3b
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-28 22:15:01 -0400

    Create test.py

---
+++ OpenAlex/test.py
@@ -0,0 +1,18 @@
+import re
+from .works import Works
+
+ref_bibtex = """author = {John R. Kitchin},
+ journal = {ACS Catalysis},
+ title = {Examples of Effective Data Sharing in Scientific Publishing},
+ volume = {5},
+ issue = {6},  
+ pages = {3894-3899},
+ year = {2015}"""
+
+title = 'Examples of Effective Data Sharing in Scientific Publishing'
+volume = 5
+issue = 6
+
+def test_bibtex():
+ w = Works("https://doi.org/10.1021/acscatal.5b00538")
+ assert title == w.new()
