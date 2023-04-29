2a3fce1c4ec03b345b9013a220246d0d3dd82366
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-28 23:02:36 -0400

    Update test.py

--- OpenAlex/test.py
+++ OpenAlex/test.py
@@ -4,13 +4,9 @@ This is the testing file
 import re
 from .works import Works
 
-ref_bibtex = """author = {John R. Kitchin},
- journal = {ACS Catalysis},
- title = {Examples of Effective Data Sharing in Scientific Publishing},
- volume = {5},
- issue = {6},  
- pages = {3894-3899},
- year = {2015}"""
+ref_bibtex = """author = John R. Kitchin,title = Examples of Effective Data Sharing in Scientific Publishing,
+volume = 5,number = , 6,pages = 3894-3899,year = 2015,doi = "https://doi.org/10.1021/acscatal.5b00538",
+url = "https://doi.org/10.1021/acscatal.5b00538",DATE_ADDED = 2023-04-25T07:12:46.013456"""
 
 title = 'Examples of Effective Data Sharing in Scientific Publishing'
 volume = 5
@@ -18,4 +14,4 @@ issue = 6
 
 def test_bibtex():
  w = Works("https://doi.org/10.1021/acscatal.5b00538")
- assert title == w.new()
+ assert ref_bibtex == w.bibtex()
