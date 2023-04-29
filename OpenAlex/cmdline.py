3ce78cef6247b88e3c671c19056868f6f8b7c132
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-28 23:57:22 -0400

    Update cmdline.py

--- OpenAlex/cmdline.py
+++ OpenAlex/cmdline.py
@@ -7,17 +7,17 @@ from .works import Works
 @click.command()
 @click.argument("doi")
 @click.option(
-    "--format",
+    "--citation_format",
     default="bibtex",
     type=click.Choice(["bibtex", "ris"]),
     help="Output format (default: bibtex)",
 )
-def citation(doi, format):
+def citation(doi, citation_format):
     """
     Outputs RIS or Bibtex format for a given Doi string.
     """
     work = Works(doi)
-    if format == "ris":
+    if citation_format == "ris":
         print(work.ris())
-    elif format == "bibtex":
+    elif citation_format == "bibtex":
         print(work.bibtex())
