5dde58f856dd0d2be9cdb7e59826c78d97ff7981
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-29 02:01:54 -0400

    Update test.py

--- OpenAlex/test.py
+++ OpenAlex/test.py
@@ -18,7 +18,7 @@ def test_bibtex():
 
 REF_RIS='''<pre>TY  - JOUR\nAU  - John R. Kitchin\nPY  - 2015\nTI  - Examples of Effective Data Sharing in Scientific Publishing\n\
 JO  - ACS Catalysis\nVL  - 5\nIS  - 6\nSP  - 3894\nEP  - 3899\nDO  - https://doi.org/10.1021/acscatal.5b00538\n\
-ER  -<pre><br><a href="data:text/plain;base64,VFkgIC0gSk9VUgpBVSAgLSBKb2huIFIuIEtpdGNoaW4KUFkgIC0gMjAxNQpUSSAgLSBFeGFtcGxlcyBvZiBFZmZlY3RpdmUgRGF0YSBTaGFyaW5nIGluIFNjaWVudGlmaWMgUHVibGlzaGluZwpKTyAgLSBBQ1MgQ2F0YWx5c2lzClZMICAtIDUKSVMgIC0gNgpTUCAgLSAzODk0CkVQICAtIDM4OTkKRE8gIC0gaHR0cHM6Ly9kb2kub3JnLzEwLjEwMjEvYWNzY2F0YWwuNWIwMDUzOApFUiAgLQ==" download="ris">Download RIS</a>'
+ER  -<pre><br><a href="data:text/plain;base64,VFkgIC0gSk9VUgpBVSAgLSBKb2huIFIuIEtpdGNoaW4KUFkgIC0gMjAxNQpUSSAgLSBFeGFtcGxlcyBvZiBFZmZlY3RpdmUgRGF0YSBTaGFyaW5nIGluIFNjaWVudGlmaWMgUHVibGlzaGluZwpKTyAgLSBBQ1MgQ2F0YWx5c2lzClZMICAtIDUKSVMgIC0gNgpTUCAgLSAzODk0CkVQICAtIDM4OTkKRE8gIC0gaHR0cHM6Ly9kb2kub3JnLzEwLjEwMjEvYWNzY2F0YWwuNWIwMDUzOApFUiAgLQ==" download="ris">Download RIS</a>'''
 
 def test_ris():
  """
