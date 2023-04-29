dccf023e5991e3125153404ca4e4103d96135ac7
JavalVyas2000 <73403218+JavalVyas2000@users.noreply.github.com>
2023-04-28 23:36:50 -0400

    Update test.py

--- OpenAlex/test.py
+++ OpenAlex/test.py
@@ -4,9 +4,15 @@ This is the testing file
 import re
 from .works import Works
 
-REF_BIBTEX = """author = John R. Kitchin,title = Examples of Effective Data Sharing in Scientific\
-Publishing,volume = 5,number = , 6,pages = 3894-3899,year = 2015,doi = "https://doi.org/10.1021/acscatal.5b00538",\
-url = "https://doi.org/10.1021/acscatal.5b00538",DATE_ADDED = 2023-04-25T07:12:46.013456"""
+REF_BIBTEX = """author = John R. Kitchin,\
+title = Examples of Effective Data Sharing in ScientificPublishing,\
+volume = 5,\
+number = , 6,\
+pages = 3894-3899,\
+year = 2015,\
+doi = "https://doi.org/10.1021/acscatal.5b00538",\
+url = "https://doi.org/10.1021/acscatal.5b00538",\
+DATE_ADDED = 2023-04-25T07:12:46.013456"""
 
 
 def test_bibtex():
